c9b134bcedeb3c87d2d1266a731ba4612373deaa
Julien DIDIER <julien@didier.io>
2014-08-10 21:02:07 +0200

    add set/get logic

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,13 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['cache.upload_dir'] = '/var/uploads';
+
+$app['db.options'] = [
+    'driver'    => 'pdo_pgsql',
+    'host'      => 'localhost',
+    'dbname'    => 'media',
+    'user'      => 'media',
+    'password'  => 'media',
+    'charset'   => 'utf8',
+];
--- src/controllers.php
+++ src/controllers.php
@@ -1,18 +1,72 @@
 <?php
 
+use Ddr\Component\Cache\Cache;
+use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-//Request::setTrustedProxies(array('127.0.0.1'));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => $app['db.options'],
+));
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array());
 })
-->bind('homepage')
-;
+->bind('homepage');
+
+$app->get('/get', function (Request $request) use ($app) {
+    $url = $request->query->get('url');
+
+    if (null === $url) {
+        throw new BadRequestHttpException('Missing url');
+    }
+
+    $sql = "SELECT * FROM content WHERE url = ?";
+    $content = $app['db']->fetchAssoc($sql, array($url));
+
+    if (false === $content) {
+        $client = new Client();
+        $response = $client->get($url);
+
+        $content = [
+            'url' => $response->getEffectiveUrl(),
+            'content_type' => $response->getHeader('content-type'),
+            'data' => $response->getBody(),
+        ];
+
+        $content['hash'] = md5($content['content_type'].$content['data']->__toString());
+
+        $cache = new Cache(__DIR__.sprintf('/../%s', $app['cache.upload_dir']));
+        $cache->set($content['hash'], $content['data']);
+
+        unset($content['data']);
+        $content['id'] = $app['db']->insert('content', $content);
+    }
+
+    return new JsonResponse($content);
+})
+->bind('get');
+
+$app->get('/{hash}', function (Request $request, $hash) use ($app) {
+    $sql = "SELECT * FROM content WHERE hash = ?";
+    $content = $app['db']->fetchAssoc($sql, array($hash));
+
+    if (false === $content) {
+        die('prout');
+    }
+
+    $cache = new Cache(__DIR__.sprintf('/../.%s', $app['cache.upload_dir']));
+    $data = $cache->get($content['hash']);
+
+    return new Response($data, 200, [
+        'content-type' => $content['content_type'],
+    ]);
+})
+->bind('post');
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
